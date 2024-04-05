766887ec329496333bc5c1196ea2afa6bad4c90d
sanjay_uzan <sanjaykhadka@Sanjays-MacBook-Pro.local>
2024-04-05 16:06:51 -0400

    Nav bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,97 @@
+import "bootstrap/js/src/collapse.js";
+import { Link } from "react-router-dom";
+
 function Navbar() {
-    const titleStyle= {
-        fontWeight: 800 
-    };
+  const titleStyle = {
+    fontWeight: 900,
+  };
   return (
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    //   <div className="container">
+    //     <a className="navbar-brand" style={titleStyle} href="#">
+    //       BookStore
+    //     </a>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-bs-toggle="collapse"
+    //       data-bs-target="#navbarToggleExternalContent"
+    //       aria-controls="navbarToggleExternalContent"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div
+    //       className="collapse navbar-collapse"
+    //       id="navbarToggleExternalContent"
+    //     >
+    //       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    //         <li className="nav-item">
+    //           <a className="nav-link active" aria-current="page" href="#">
+    //             Home
+    //           </a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="#">
+    //             Features
+    //           </a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="#">
+    //             Pricing
+    //           </a>
+    //         </li>
+    //       </ul>
+    //       <p>
+    //         <i className="fa fa-solid fa-book"></i>
+    //       </p>
+    //       <button className="btn btn-primary">Login</button>
+    //     </div>
+    //   </div>
+    // </nav>
+
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <a className="navbar-brand" style={titleStyle} href="#">
-          BookStore
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarToggleExternalContent"
-          aria-controls="navbarToggleExternalContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-          </ul>
-          <p><i className="fa fa-solid fa-book"></i></p>
-         <button className="btn btn-primary">Login</button>
+    <div className="container">
+      <a className="navbar-brand" style={titleStyle} href="#">
+        Bookstore
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse align-left justify-content-lg-between" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+        </ul>
+        
+        <div className="d-flex gap-4 align-items-center">
+        <a href="#!" class="text-white"><i class="fa fa-book fa-lg"></i> <i class="fa fa-caret-down"></i></a>
+        <Link className="btn btn-primary" to={"/login"}>Login</Link>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
+
+    
+
+
+
+
+
+
+
   );
 }
 
